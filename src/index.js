c0f261e3c2c56faf9344ed8769ddf5af8ab3d4e3
KnightNoder <lavakumar@codeastra.com>
2022-01-25 20:49:53 +0530

    icon, hbs,partials and views added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-require('dotenv').config({ path: '.env' });
-require('./db/conn');
+// require('dotenv').config({ path: '.env' });
+// require('./db/conn');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+const hbs = require('hbs');
+const favicon = require('serve-favicon');
 
-app.listen(() => {
+app.use(favicon(path.join(__dirname, '../public/icons/logo.ico')));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../public/templates/views'));
+hbs.registerPartials(path.join(__dirname, '../public/templates/partials'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+app.listen(PORT, () => {
   console.log(`listening to server on ${PORT}`);
 });
